373d2708ebff4e54ce9d604f7b75d848304ddef6
akhileshjaguri-dev <akhileshjaguri02@gmail.com>
2024-02-16 10:43:33 +0530

    app & middleware setup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import dotenv from "dotenv"
 import { app } from "./app.js"
-import connectDB from "./db/index.js";
+import connectDB from "./db/index.js"
 dotenv.config({
     path: './.env'
 })
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 connectDB().then(() => {
     app.listen(PORT , () => {
-        console.log(`Server running on prot : ${PORT}`);
+        console.log(`Server running on prot : ${PORT}`)
     })
 })
 .catch((error) => {
-    console.log("MONGO db connection failed !!! ", err);
+    console.log("MONGO db connection failed !!! ", err)
 })
 
